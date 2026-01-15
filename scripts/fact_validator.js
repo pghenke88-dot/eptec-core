45b4d3a67968043eef656b09007f2145cf9ad530
pghenke88-dot <pghenke88@gmail.com>
2026-01-15 17:35:16 +0100

    Create fact_validator.js

---
+++ scripts/fact_validator.js
@@ -0,0 +1,17 @@
+// EPTEC Fakten-Validator (Sicherung der Nutzer-Autonomie)
+const FactValidator = {
+    confirmDiscrepancy: (pointId) => {
+        const checkText = "Ich bestätige hiermit, dass ich die vom System gemeldete " +
+                          "Abweichung bei Punkt " + pointId + " selbst geprüft habe " +
+                          "und diese als Tatsache feststelle.";
+        
+        // Der Nutzer MUSS aktiv klicken, bevor die Ampel umschaltet
+        if (confirm(checkText)) {
+            console.log("Nutzer hat die Abweichung als Tatsache bestätigt.");
+            return true;
+        } else {
+            console.log("Nutzer hat die Bestätigung verweigert. Ampel bleibt Gelb.");
+            return false;
+        }
+    }
+};
