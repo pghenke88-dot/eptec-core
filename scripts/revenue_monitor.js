aac2741be79a44e8e493d39bc6112612eeba20a9
pghenke88-dot <pghenke88@gmail.com>
2026-01-27 22:15:56 +0100

    Update revenue_monitor.js

--- scripts/revenue_monitor.js
+++ scripts/revenue_monitor.js
@@ -1,4 +1,4 @@
-// EPTEC Finanz-Monitor
+try {// EPTEC Finanz-Monitor
 function checkRevenue(expectedAmount, actualTransactions) {
     // Suche in den Bankdaten nach dem Betrag
     const paymentFound = actualTransactions.find(t => t.amount >= expectedAmount);
@@ -10,3 +10,5 @@ function checkRevenue(expectedAmount, actualTransactions) {
         return "RED";
     }
 }
+
+} catch (e) { console.error("Fehler:", e); }
